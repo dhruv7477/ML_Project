4239050dff31254ee794d2200fbbd6f02c7bc957
Dhruv Sharma <dhruv7477@gmail.com>
2023-01-05 15:52:31 +0530

    Updated!

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 #Declaring variables for setup functions
@@ -19,7 +19,7 @@ def get_requirements_list()->List[str]:
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 
 setup(
@@ -27,7 +27,6 @@ name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESRCIPTION,
-packages=PACKAGES,
+packages=find_packages(), 
 install_requires=get_requirements_list()
-
 )
