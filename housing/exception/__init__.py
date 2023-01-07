9c202142d95d31bbc8106c45666a02bbb26b7115
Dhruv Sharma <dhruv7477@gmail.com>
2023-01-07 12:54:37 +0530

    Added new files!

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -3,7 +3,7 @@ import sys
 
 class HousingException(Exception):
     
-    def __init__(self, error_message:Exception,error_detail:sys):
+ def __init__(self, error_message:Exception,error_detail:sys):
         super().__init__(error_message)
         self.error_message=HousingException.get_detailed_error_message(error_message=error_message,
                                                                        error_detail=error_detail
@@ -19,7 +19,7 @@ class HousingException(Exception):
         _,_ ,exec_tb = error_detail.exc_info()
         line_number = exec_tb.tb_frame.f_lineno
         file_name = exec_tb.tb_frame.f_code.co_filename
-        error_message = f"Error occured in scrip: [{file_name}] at line number: [{line_number}] error message: [{error_message}]"
+        error_message = f"Error occured in script: [{file_name}] at line number: [{line_number}] error message: [{error_message}]"
         return error_message
 
     def __str__(self):
