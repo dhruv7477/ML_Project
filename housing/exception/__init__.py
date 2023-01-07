f8e8ce542dbf7e72e6e663449dc50c3b743a5991
Dhruv Sharma <dhruv7477@gmail.com>
2023-01-07 16:54:58 +0530

    Added/updated some files!

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -3,7 +3,7 @@ import sys
 
 class HousingException(Exception):
     
- def __init__(self, error_message:Exception,error_detail:sys):
+    def __init__(self, error_message:Exception,error_detail:sys):
         super().__init__(error_message)
         self.error_message=HousingException.get_detailed_error_message(error_message=error_message,
                                                                        error_detail=error_detail
