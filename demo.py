e7f299e85ce80800ca8fea62da64ba1d402e0d55
Dhruv Sharma <dhruv7477@gmail.com>
2023-01-09 15:04:00 +0530

    Updated!

--- demo.py
+++ demo.py
@@ -1,15 +1,16 @@
 from housing.pipeline.pipeline import Pipeline
 from housing.exception import HousingException
 from housing.logger import logging
+from housing.config.configuration import Configuartion
 def main():
     try:
-        pipeline = Pipeline()
-        pipeline.run_pipeline()
+        # pipeline = Pipeline()
+        # pipeline.run_pipeline()
+        data_validation_config = Configuartion().get_data_validation_config()
+        print(data_validation_config)
     except Exception as e:
         logging.error(f"{e}")
         print(e)
-
-
 
 if __name__=="__main__":
     main()
